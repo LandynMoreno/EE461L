ffb8df6acef0a56bd5be5e1274a7fb592baccd83
Jason Dsouza <jrdsouza3@gmail.com>
2022-04-04 02:27:29 -0500

    all f end, issues w/ sending data to flask

--- flask/flServer.py
+++ flask/flServer.py
@@ -1,12 +1,26 @@
 from distutils.log import debug
 from flask import Flask, render_template, request, redirect
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/people")
 def users():
     return {"users": ["jason", "john ", "jose"]}
+
+
+@app.route("/logcheck", methods = ["POST"])
+def checker():
+    given = request.get_json
+    print(given)
+    username = given["user"]
+    pswd = given["pswd"]
+
+    return{
+        "lastname": "asdasdasd"
+    }
 
 
 if __name__ == "__main__":
--- project/src/components/Signin.js
+++ project/src/components/Signin.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import { Button, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom'
 
 function Signin() {
     const navigate = useNavigate()
+    const [errorMsg, setError] = useState("initial error message")
+    const [usernm, setUser] = useState("")
+    const [pswd, setPswd] = useState("")
+
+    const updateUser = (newUser) => {
+        setUser(newUser.target.value)
+        //console.log(usernm)
+        
+    }
+
+    const updatePswd = (newPswd) => {
+        setPswd(newPswd.target.value)
+      
+    }
+
+    const send = () => {
+        console.log("Sending")
+        const tosend = { usernm };
+        const response = fetch("/logcheck", {
+     
+            // Adding method type
+            method: "POST",
+             
+            // Adding body or contents to send
+            body: JSON.stringify({
+                title: "foo",
+                body: "bar",
+                userId: 1
+            }),
+             
+            // Adding headers to the request
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+ 
+        
+
+        //navigate('/projects')
+    }
 
 
     return (
@@ -11,13 +51,14 @@ function Signin() {
             <h1>
                 Username
             </h1>
-            <TextField id="outlined-basic" label="Username" variant="outlined" />
+            <TextField value = {usernm} d="outlined-basic" label="Username" variant="outlined" onChange={updateUser} />
             <h1>
                 Password
-            </h1>
-            <TextField id="outlined-basic" label="Password" variant="outlined" />
-            <Button variant="contained">Submit</Button>
+            </h1>       
+            <TextField value = {pswd} id="outlined-basic" label="Password" variant="outlined" onChange= {updatePswd} />
+            <Button variant="contained" onClick={send} >Submit</Button>
             <Button variant="contained" onClick={()=>navigate('/newuser')}>New User????</Button>
+            <h3> {errorMsg} </h3>
 
 
             
