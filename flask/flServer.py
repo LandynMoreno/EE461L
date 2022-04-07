9ef26139b5457284a52bf097bdbf3f47c10441c1
Jason Dsouza <jrdsouza3@gmail.com>
2022-04-07 03:32:57 -0500

    Connection to mongo user collec

--- flask/flServer.py
+++ flask/flServer.py
@@ -1,5 +1,5 @@
 from distutils.log import debug
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_cors import CORS;
 from flask_pymongo import PyMongo
 from users import Users
@@ -9,10 +9,11 @@ from users import Users
 
 app = Flask(__name__)
 CORS(app)
-client = MongoClient("mongodb+srv://jrd4455:sadfsadf@cluster0.wsoqa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-#password is TEST123
-db = client["project"]
-users = db["users"]
+client = PyMongo(app, uri="mongodb+srv://tester:helloworld@cluster0.wsoqa.mongodb.net/project?retryWrites=true&w=majority")
+#password is TEST123 for jrd
+db = client.db
+userCollec = db.users
+
 
 @app.route("/people")
 def users():
@@ -28,23 +29,27 @@ def checker():
         "message": "approved"
     }
 
-@app.route("/adduser", methods = ["PUT"]) #get and post
+@app.route("/adduser", methods = ["POST", "GET"]) #get and post
 def addPerson():
-    #given = request.get_json
+    given = request.get_json()
+    usernameParam = given['username']
+    pswrdParam = given['password']
 
-    db1 = users.User()
+
+
+    #db1 = users.User()
     #db1.addUser(given["username"], given["password"])
 
     newDoc = {
-        "username": "asdasd",
-        "password": "asdasda"
+        "username": usernameParam,
+        "password": pswrdParam
     }
 
-    #users.insert_one(newDoc)
+    userCollec.insert_one(newDoc)
 
 
     return{
-        "message": "approved"
+        "message": "asdasd"
     }
 
 
