a4083591dd4c286a10326413cc4ab0f9d129c9b7
Jason D <jrdsouza3@gmail.com>
2022-04-16 12:51:00 -0500

    proj changes

--- project/src/components/Projects.js
+++ project/src/components/Projects.js
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, TextField } from '@mui/material';
 import { useNavigate } from 'react-router-dom'
 
 function Projects({globalUser, setGlobalUser}) {
     const navigate = useNavigate()
 
+    const [projName, setName] = useState("")
+    const [projId, setId] = useState("")
+    const [projDescrip, setDescrip] = useState("")
+    const[searchId, setSearchId] = useState("")
+    const[error, setError] = useState("")
+
+    const updateName = (newName) => {
+        setName(newName.target.value)  }
+    const updateId= (newId) => {
+        setId(newId.target.value)  }
+    const updateDescrip = (newDescrip) => {
+        setDescrip(newDescrip.target.value)  }
+    const updateSearchid = (newSearch) => {
+        setSearchId(newSearch.target.value)  }
+
+    const sendNewProj = () => {
+        const sent = {        
+            method: "POST",
+            headers: {'Content-Type': 'application/json',
+                    'Accept': 'application/json'},
+            body: JSON.stringify(
+                {name: projName,
+                description: projDescrip,
+                id: projId
+            })
+        }
+        if(projName.length < 1 )
+        {
+            setError("Name must be non-zero length")
+
+        }
+        else if (projDescrip.length < 1)
+        {
+            setError("description must be non-zero length")
+        }
+        else if(projId.length < 1 )
+        {
+            setError("ID must be non-zero length")
+        }
+        else{
+            console.log("sending")
+            //add the send here
+        }
+
+
+
+    }
+    const checkProj = () =>{
+
+        const sent = {        
+            method: "POST",
+            headers: {'Content-Type': 'application/json',
+                    'Accept': 'application/json'},
+            body: JSON.stringify(
+                { id: searchId
+            })
+        }
+        if(searchId.length < 1)
+        {
+            setError("ID must be a non-zero length")
+        }
+        else{
+            console.log("Sendings")
+            // add the send here
+
+        
+        }
+
+
+    }
+
+    
 
     return (
         <div>
@@ -13,15 +85,15 @@ function Projects({globalUser, setGlobalUser}) {
             </h1>
             <h3> {globalUser}</h3>
             <p>Create New Project</p>
-            <TextField id="outlined-basic" label="Project Name" variant="outlined" />
-            <TextField id="outlined-basic" label="Project Description" variant="outlined" />
-            <TextField id="outlined-basic" label="ProjectID" variant="outlined" />
-            <Button variant="contained" onClick={()=>navigate('/ResourceManagement')}>Create</Button>
+            <TextField value = {projName} id="outlined-basic" label="Project Name" variant="outlined" onChange={updateName}/>
+            <TextField value ={projDescrip} id="outlined-basic" label="Project Description" variant="outlined" onChange={updateDescrip}/>
+            <TextField value = {projId} id="outlined-basic" label="ProjectID" variant="outlined" onChange = {updateId}/>
+            <Button variant="contained" onClick={sendNewProj}>Create</Button>
 
             <p>Use existing Project</p>
-            <TextField id="outlined-basic" label="ProjectID" variant="outlined" />
-            <Button variant="contained" onClick={()=>navigate('/ResourceManagement')}>Access Project</Button>
-
+            <TextField value = {searchId} id="outlined-basic" label="ProjectID" variant="outlined" onChange={updateSearchid}/>
+            <Button variant="contained" onClick={checkProj}>Access Project</Button>
+            <h3> {error} </h3>
             
         </div>
     );
